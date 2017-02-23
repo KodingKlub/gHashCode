d19711146ab34691ac414dc7c9abfc4bcccc351b
Philipp Pajak <espspinix@gmail.com>
2017-02-23 20:33:06 +0100

    added savings calc

--- philipp.py
+++ philipp.py
@@ -9,12 +9,18 @@ if __name__ == '__main__':
     fname = sys.argv[1] if len(sys.argv) == 2 else "data/small.in"
     problem = parser.get_problem(fname)
 
-		endpoints_example = len(problem.endpoints)
-		endpoint_savings = []
-		for endpoint in range(endpoint_savings):
-				savings = np.zeros(len(problem.videos),len(problem.num_caches))
-				for request in problem.endpoints.requests:
-					for cache in problem.endpoint.caches:
-						cache_saving = endpoint.datacenterlatency - cache.latency
-						savings[request.video,cache.id] = request.amount * (cache_savings)
-				endpoint_savings.append(savings)
+    endpoints_example = len(problem.endpoints)
+    endpoint_savings = {}
+    for endpoint in problem.endpoints:
+        savings = np.ones((len(problem.video_sizes), problem.num_caches))
+        savings *= -1
+
+        for request in problem.get_requests_for_endpoint(endpoint.id):
+            for cache in endpoint.cache_connections:
+                cache_savings = endpoint.dc_latency - cache.latency
+                savings[request.video_id,cache.cache_id] = request.num_requests * (cache_savings)
+
+        endpoint_savings[endpoint] = savings
+
+
+    print(endpoint_savings)
