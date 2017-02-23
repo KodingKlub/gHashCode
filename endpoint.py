7acfb2da63d70d09d65f4c168d10ff65144bbc53
Wojciech Lukasiewicz <w.l@fu-berlin.de>
2017-02-23 20:29:47 +0100

    renamed var in endpointdescription

--- endpoint.py
+++ endpoint.py
@@ -1,7 +1,7 @@
 class EndpointDescription:
-    def __init__(self, id, dc_latency, num_cache_connections, cache_latencies):
+    def __init__(self, id, dc_latency, num_cache_connections, cache_connections):
         self.id = id
         self.dc_latency = dc_latency
         self.num_cache_connections = num_cache_connections
-        self.cache_latencies = cache_latencies
+        self.cache_connections = cache_connections
         self.requests = []
